b2bc095dd853ef76c595c77593bd0d97628b5956
Joseph Davis <soccermaniac898@gmail.com>
2018-04-18 14:05:39 -0500

    Add dummy components

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => {
+  return <h2>Header</h2>;
+};
+
+const Dashboard = () => {
+  return <h2>Dashboard</h2>;
+};
+
+const SurveyNew = () => {
+  return <h2>SurveyNew</h2>;
+};
+
+const Landing = () => {
+  return <h2>Landing</h2>;
+};
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
